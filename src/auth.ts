a921fe5b4378eced52717fc045f8b6da68a1fea8
Panuwat Handsome <panuwat.c@ddlg-corp.com>
2025-09-01 00:00:12 +0700

    up

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma";
@@ -12,7 +14,7 @@ const credSchema = z.object({
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [
+  providers: [GitHub,Google,
     Credentials({
       credentials: {
         email: { label: "Email", type: "text" },
